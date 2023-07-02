ebdc6dca83bc2b25ab520047be84c16a1bd31dce
cclabdis <cclabdis@gmail.com>
2023-07-02 20:09:27 -0300

    add enfpoint post message

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ const participantsSchema = joi.object({
 })
 
 const messagesSchema = joi.object({
+    from: joi.string().required(),
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required().valid("private_message", "message")
 
 })
 
@@ -22,7 +26,7 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
 
-try{
+try {
     await mongoClient.connect()
     db = mongoClient.db()
 } catch (error) {
@@ -31,7 +35,7 @@ try{
 
 }
 
- 
+
 
 //O formato de um participante deve ser:
 //{name: 'João', lastStatus: 12313123} // O conteúdo do lastStatus será explicado nos próximos requisitos
@@ -40,35 +44,35 @@ try{
 
 //endpoints
 
- server.post('/participants', async (req, res) => {
+server.post('/participants', async (req, res) => {
     const { name } = req.body
-    const { error } = participantsSchema.validate({ name }, {abortEarly: false})
+    const { error } = participantsSchema.validate({ name }, { abortEarly: false })
 
-    if(error){
+    if (error) {
         return res.status(422).send('Unprocessable Entity')
-    } 
+    }
 
     const datenow = Date.now()
 
-    try{
-       const duplicate = await db.collection("participants").findOne({name: name})
-       if(duplicate){
-        return res.status(409).send('Conflict')
-       }
+    try {
+        const duplicate = await db.collection("participants").findOne({ name: name })
+        if (duplicate) {
+            return res.status(409).send('Conflict')
+        }
 
-       await db.collection("participants").insertOne({name: name, lastStatus: datenow })
+        await db.collection("participants").insertOne({ name: name, lastStatus: datenow })
 
-       await db.collection("messages").insertOne({
-        from: name,
-        to: 'Todos',
-        text: 'entra na sala...',
-        type: 'status',
-        time: dayjs().format("HH:mm:ss")
-       })
+        await db.collection("messages").insertOne({
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format("HH:mm:ss")
+        })
 
-       res.status(201).send('Created')
-       // O formato de uma mensagem deve ser:
-// {from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}
+        res.status(201).send('Created')
+        // O formato de uma mensagem deve ser:
+        // {from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}
 
     } catch (error) {
         res.sendStatus(500)
@@ -88,9 +92,31 @@ server.get('/participants', async (req, res) => {
 
 });
 
-server.post('/messages', (req, res) => {
+server.post('/messages', async (req, res) => {
     const { to, text, type } = req.body
     const { user } = req.headers
+
+    const { error } = messagesSchema.validate({ from: user, to, text, type }, { abortEarly: false })
+
+    if (error) {
+        return res.status(422).send('Unprocessable Entity')
+    }
+
+    try {
+        await db.collection("messages").insertOne({
+            from: user,
+            to,
+            text ,
+            type ,
+            time: dayjs().format("HH:mm:ss")
+        })
+
+        res.status(201).send('Created')
+
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(500)
+    }
 
 });
 
@@ -107,6 +133,6 @@ const PORT = 5000
 
 server.listen(PORT, () => {
     console.log("Rodando")
-  });
-  
+});
+
   //fuser -k 4000/tcp//
