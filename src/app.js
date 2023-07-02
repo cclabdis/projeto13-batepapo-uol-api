2f91b307d72270db4c01ae22656641d500c750a1
cclabdis <cclabdis@gmail.com>
2023-07-02 19:12:05 -0300

    add endpoint post participants

--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@ import express, { json } from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
+import joi from 'joi';
+import dayjs from 'dayjs'
 dotenv.config();
+
+const participantsSchema = joi.object({
+    name: joi.string().required()
+})
 
 const server = express();
 server.use(cors());
@@ -11,23 +17,60 @@ server.use(json());
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
-mongoClient.connect()
- .then(() => db = mongoClient.db())
- .catch((err) => console.log(err.message));
 
+try{
+    await mongoClient.connect()
+    db = mongoClient.db()
+} catch (error) {
+    console.error(error)
+    console.log('nao conseguiu se conectar ao servidor')
 
+}
+
+ 
 
 //O formato de um participante deve ser:
 //{name: 'João', lastStatus: 12313123} // O conteúdo do lastStatus será explicado nos próximos requisitos
-// O formato de uma mensagem deve ser:
-// {from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}
+
 
 
 //endpoints
 
-// server.post('/participants', (req, res) => {
+ server.post('/participants', async (req, res) => {
+    const { name } = req.body
+    const { error } = participantsSchema.validate({ name }, {abortEarly: false})
 
-// });
+    if(error){
+        return res.status(422).send('Unprocessable Entity')
+    } 
+
+    const datenow = Date.now()
+
+    try{
+       const duplicate = await db.collection("participants").findOne({name: name})
+       if(duplicate){
+        return res.status(409).send('Conflict')
+       }
+
+       await db.collection("participants").insertOne({name: name, lastStatus: datenow })
+
+       await db.collection("messages").insertOne({
+        from: name,
+        to: 'Todos',
+        text: 'Entrou na sala...',
+        type: 'status',
+        time: dayjs(datenow).format("HH:MM:SS")
+       })
+
+       res.status(201).send('Created')
+       // O formato de uma mensagem deve ser:
+// {from: 'João', to: 'Todos', text: 'oi galera', type: 'message', time: '20:04:37'}
+
+    } catch (error) {
+        res.sendStatus(500)
+    }
+
+});
 
 // server.get('/participants', (req, res) => {
 
@@ -45,8 +88,11 @@ mongoClient.connect()
 
 // });
 
-server.listen(5000, () => {
-    console.log("Rodando em http://localhost:5000");
+
+const PORT = 5000
+
+server.listen(PORT, () => {
+    console.log("Rodando");
   });
   
   //fuser -k 4000/tcp//
